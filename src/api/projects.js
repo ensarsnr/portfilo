f948d6e1ad68ab79f326e9f896b250881f289a8b
ensarsnr <dev.anise@outlook.com>
2023-12-07 00:50:47 +0300

    Project properties added

---
+++ src/api/projects.js
@@ -0,0 +1,25 @@
+const projects = [
+  {
+    name: "Docker-Website",
+    url: "https://github.com/ensarsnr/Docker-Website",
+    desc: "Using docker's deploy process with an interface via the browser.",
+    img: "https://private-user-images.githubusercontent.com/113799443/271508860-129788e8-9a78-47ac-9652-ae5a8b3941ac.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDE4OTg1MTcsIm5iZiI6MTcwMTg5ODIxNywicGF0aCI6Ii8xMTM3OTk0NDMvMjcxNTA4ODYwLTEyOTc4OGU4LTlhNzgtNDdhYy05NjUyLWFlNWE4YjM5NDFhYy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMxMjA2JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMTIwNlQyMTMwMTdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1kYWU4MTNjOGI0ODFmMjg2NGIzYzQwMGZlMDdmZDg3NDNmMjg4MDRjNTQyMWVkNzdjZWQwYWEzNTMxYjBkODdkJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.uymYxJnKogBm_6i3ij2P6B5xanVth6UabeIUKnpXvHU",
+    techs: ["NodeJS", "Linux", "Tailwind", "Docker", "React", "ExpressJS"],
+  },
+  {
+    name: "Chat Application",
+    url: "https://github.com/ensarsnr/chat-app-frontend",
+    desc: "I made a real time chat application. I used Backend and Database.",
+    img: "https://private-user-images.githubusercontent.com/113799443/288551952-9538d8da-f9a7-44a4-a1a7-b860cfd62175.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDE4OTg2MTAsIm5iZiI6MTcwMTg5ODMxMCwicGF0aCI6Ii8xMTM3OTk0NDMvMjg4NTUxOTUyLTk1MzhkOGRhLWY5YTctNDRhNC1hMWE3LWI4NjBjZmQ2MjE3NS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMxMjA2JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMTIwNlQyMTMxNTBaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1kMWE0YTRkMTYwZDZjMWY1ZjAyZjcxNjc5OWZlMDdmZWIxYWI3MzhiMDJhNjhmZjEyNjhkOTFkOWQyNjgxMzMxJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.au9qj-i6HNaIVd4-X1IMd-gS7r8JKuNo_4_6LnC4mNA",
+    techs: ["NextJS", "Tailwind", "MongoDB", "NodeJS", "ExpressJS"],
+  },
+  {
+    name: "Beatiful React Hooks",
+    url: "https://github.com/ensarsnr/beautiful-react-hooks",
+    desc: "I provided Turkish support to a react special hook library that I liked.",
+    img: "https://github.com/antonioru/beautiful-react-hooks/raw/master/logo.png",
+    techs: ["README.md"],
+  },
+];
+
+export default projects;
