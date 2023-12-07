a21548da4d803f52ce9b094cfd1ed6694c0b9679
ensarsnr <dev.anise@outlook.com>
2023-12-07 23:12:04 +0300

    Created picture page

---
+++ src/pages/Pictures.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Pictures() {
+  return (
+    <>
+      <div className="flex h-screen">
+        <div className="m-auto">
+          <h1 className="sm:text-9xl text-2xl">My Pictures</h1>
+          <div className="text-center mt-5">
+            <span className="m-5">asdads</span>
+            <span className="m-5">asdads</span>
+            <span className="m-5">asdads</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Pictures;
