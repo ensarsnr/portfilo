af2aaba43381515b03e07d1871ee593ae054ef1b
ensarsnr <dev.anise@outlook.com>
2023-12-07 23:11:06 +0300

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Pictures from "./pages/Pictures";
 import Blog from "./pages/Blog";
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="project" element={<Projects />}/>
-        <Route path="blog" element={<Blog />} />
+        <Route path="project" element={<Projects />} />
+        {/* <Route path="pictures" element={<Pictures />} /> */}
+        {/* <Route path="blog" element={<Blog />} /> */}
       </Routes>
     </BrowserRouter>
   );
